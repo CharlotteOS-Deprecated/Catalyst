43e486d11b22f7803690d3b54c3fbd616f96935a
Mohit D Patel <mdpatel.csecon@outlook.com>
2023-11-21 18:59:00 -0500

    Created a function to calculate the size of the physical memory bitmap and find a location in physical memory to put it

--- kernel/memory/frame_alloc.c
+++ kernel/memory/frame_alloc.c
@@ -1,15 +1,39 @@
 #include "memory/include/frame_alloc.h"
+#include "limine.h"
 
+#include <stddef.h>
 #include <stdint.h>
 
 typedef uint64_t paddr_t;
 typedef void* vaddr_t;
 
-//The number of page frames reserved for use by the allocator and their physical base address
-[[maybe_unused]]
-static uint64_t N_FRAMES_RESERVED = 0;
-[[maybe_unused]]
-static uint64_t RESERVED_BADDR = 0;
+//The number of page frames reserved for the physical memory bitmap and its physical base address
+static uint64_t BMAP_NFRAMES = 0;
+static uint64_t BMAP_PADDR = 0;
 
-//Use a linked list with structs describing blocks of contiguous frames
+void alloc_pmem_bmap(const struct limine_memmap_entry *const *const entries, const size_t n_entries)
+{
+        size_t n_total_frames = 0;
+
+        for(size_t i = 0; i < n_entries; ++i) {
+                if (entries[i]->type == LIMINE_MEMMAP_USABLE) {
+                        n_total_frames += entries[i]->length / PAGE_SZ;
+                }
+        }
+        //Calculate the size of the physical memory bitmap in bytes (one bit per page frame)
+        size_t bmap_sz = (n_total_frames / 8);
+        if (n_total_frames % 8 > 0)
+                ++bmap_sz;
+        //Calculate the number of page frames to reserve for the physical memory bit map
+        BMAP_NFRAMES = (bmap_sz / 4096);
+        if (n_total_frames % PAGE_SZ > 0)
+                ++bmap_sz;
+        //find and reserve bmap_nframes page frames
+        for(size_t i = 0; i < n_entries; ++i) {
+                if (entries[i]->type == LIMINE_MEMMAP_USABLE && entries[i]->length > bmap_sz) {
+                        BMAP_PADDR = entries[i]->base;
+                }
+        }
+        //TODO: Handle the case where no simgle memmap entry has enough memory for the entire bitmap
+}
 
