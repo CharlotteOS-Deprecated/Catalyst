3f73a2980ead6a12ea4e70132c5c6e7fe79077d9
ThatOSDeveloper <owen.winchell@proton.me>
2024-09-08 20:56:54 -0400

    minor updates to fix issues with code in headers (more work will be done on this later)

---
+++ kernel/panic/api.c
@@ -0,0 +1,22 @@
+#include <stdbool.h>
+#include "arch/api.h"
+#include "log/log.h"
+
+void panic(void) {
+	log_puts("A kernel panic has occurred!!!");
+	isa_hcf();
+}
+
+void assert([[maybe_unused]] bool expr) {
+#ifdef ASSERT
+	if (expr == false)
+		panic();
+#endif
+}
+
+void assert_not_null([[maybe_unused]] const void *const ptr) {
+#ifdef ASSERT
+	if (ptr == nullptr)
+		panic();
+#endif
+}
--- kernel/panic/api.h
+++ kernel/panic/api.h
@@ -1,29 +1,8 @@
 #ifndef PANIC_API_H
 #define PANIC_API_H
 
-#include "arch/api.h"
-#include "log/log.h"
-
-inline void panic(void)
-{
-	log_puts("A kernel panic has occurred!!!");
-	isa_hcf();
-}
-
-inline void assert([[maybe_unused]] const bool expr)
-{
-#ifdef ASSERT
-	if (expr == false)
-		panic();
-#endif
-}
-
-inline void assert_not_null([[maybe_unused]] const void *const ptr)
-{
-#ifdef ASSERT
-	if (ptr == nullptr)
-		panic();
-#endif
-}
+void panic(void);
+void assert([[maybe_unused]] const bool expr);
+void assert_not_null([[maybe_unused]] const void *const ptr);
 
 #endif
