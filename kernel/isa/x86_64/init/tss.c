46af24ab714c0d7a678c459627db16faddedb1b2
Mohit D Patel <mdpatel.csecon@outlook.com>
2025-06-29 02:01:04 -0400

    Correct paths broken by the x86_64 directory refactor
    Signed-off-by: Mohit D Patel <mdpatel.csecon@outlook.com>

--- kernel/isa/x86_64/init/tss.c
+++ kernel/isa/x86_64/init/tss.c
@@ -16,7 +16,7 @@ You should have received a copy of the GNU General Public License
 along with this program.  If not, see https://www.gnu.org/licenses/
 */
 
-#include "isa/x86_64/tss.h"
+#include "isa/x86_64/init/tss.h"
 
 #include "lib/string/string.h"
 
@@ -24,18 +24,18 @@ along with this program.  If not, see https://www.gnu.org/licenses/
 
 void setup_tss(tss_t tss, void *const rsp0)
 {
-	// Zero out the TSS
-	utility_memset(tss, 0, sizeof(uint32_t) * 27);
+    // Zero out the TSS
+    utility_memset(tss, 0, sizeof(uint32_t) * 27);
 
-	// Set the value of the kernel stack
-	uint64_t rsp0_uint = (uint64_t)rsp0;
+    // Set the value of the kernel stack
+    uint64_t rsp0_uint = (uint64_t)rsp0;
 
-	tss[1] = (uint32_t)(rsp0_uint & 0xFFFFFFFF);
-	tss[2] = (uint32_t)((rsp0_uint & 0xFFFFFFFF00000000) >> 32);
+    tss[1] = (uint32_t)(rsp0_uint & 0xFFFFFFFF);
+    tss[2] = (uint32_t)((rsp0_uint & 0xFFFFFFFF00000000) >> 32);
 
-	// Set the offset of the IOPB
-	tss[26] = 104 << 16;
-	// Set the first byte of the IOPB to the end byte (all 1s) to signal that the IOPB will not
-	// be used
-	tss[27] = 0xFF;
+    // Set the offset of the IOPB
+    tss[26] = 104 << 16;
+    // Set the first byte of the IOPB to the end byte (all 1s) to signal that the IOPB will not
+    // be used
+    tss[27] = 0xFF;
 }
--- kernel/isa/x86_64/interrupts/exceptions.c
+++ kernel/isa/x86_64/interrupts/exceptions.c
@@ -1,10 +1,10 @@
-#include "isa/x86_64/exceptions.h"
+#include "isa/x86_64/interrupts/exceptions.h"
 
 // freestanding
 #include <stddef.h>
 
 // in-module
-#include "cpu.h"
+#include "isa/x86_64/lp_control/cpu.h"
 
 // out-of-module
 #include "lib/string/string.h"
@@ -20,57 +20,57 @@ These have no forward declarations because they are only ever called from assemb
 
 void ih_divide_by_zero(void)
 {
-	log_puts("A divide by zero exception has occurred in kernelspace!\r\nPanicking!\r\n");
-	panic();
+    log_puts("A divide by zero exception has occurred in kernelspace!\r\nPanicking!\r\n");
+    panic();
 }
 
 void ih_overflow(void)
 {
-	log_puts("An overflow exception occurred!\r\n");
+    log_puts("An overflow exception occurred!\r\n");
 }
 
 void ih_invalid_opcode(void)
 {
-	log_puts("An invalid opcode exception occurred!\r\n");
-	panic();
+    log_puts("An invalid opcode exception occurred!\r\n");
+    panic();
 }
 
 void ih_general_protection_fault(const uint64_t int_err_code)
 {
-	log_puts("A general protection fault has occurred\r\n");
-	log_puts("Error Code: ");
-	char temp_str[21];
-	utility_u64_to_dec_str(int_err_code, temp_str);
-	log_puts(temp_str);
-	log_puts("\r\n");
+    log_puts("A general protection fault has occurred\r\n");
+    log_puts("Error Code: ");
+    char temp_str[21];
+    utility_u64_to_dec_str(int_err_code, temp_str);
+    log_puts(temp_str);
+    log_puts("\r\n");
 
-	panic();
-	// TODO: Handle actual cases that can cause GP exceptions
+    panic();
+    // TODO: Handle actual cases that can cause GP exceptions
 }
 
 void ih_double_fault(const uint64_t int_err_code)
 {
-	(void)int_err_code; // This value is always 0 and so not needed
-	log_puts("A double fault has ocurred!\r\nPanicking!\r\n");
-	panic();
+    (void)int_err_code; // This value is always 0 and so not needed
+    log_puts("A double fault has ocurred!\r\nPanicking!\r\n");
+    panic();
 }
 
 void ih_page_fault(const uint64_t int_err_code)
 {
-	log_puts("A page fault has occurred\r\n");
-	log_puts("Error Code: ");
-	char temp_str[21];
-	utility_memset(temp_str, 0, 21);
-	utility_u64_to_hex_str(int_err_code, temp_str);
-	log_puts(temp_str);
-	log_puts("\r\n");
-	get_regs();
-	log_puts("Registers:\r\n");
-	for (size_t i = 0; i < 16; ++i) {
-		utility_memset(temp_str, 0, 21);
-		utility_u64_to_hex_str(g_cpu_regs[i], temp_str);
-		log_puts(temp_str);
-		log_puts("\r\n");
-	}
-	panic();
+    log_puts("A page fault has occurred\r\n");
+    log_puts("Error Code: ");
+    char temp_str[21];
+    utility_memset(temp_str, 0, 21);
+    utility_u64_to_hex_str(int_err_code, temp_str);
+    log_puts(temp_str);
+    log_puts("\r\n");
+    get_regs();
+    log_puts("Registers:\r\n");
+    for (size_t i = 0; i < 16; ++i) {
+        utility_memset(temp_str, 0, 21);
+        utility_u64_to_hex_str(g_cpu_regs[i], temp_str);
+        log_puts(temp_str);
+        log_puts("\r\n");
+    }
+    panic();
 }
